bc9c965aee2a1b095ab909aead6aa8c5ded5d0ea
Fred <domaine75@gmail.com>
2019-03-13 22:07:17 +0100

    Resharper code

--- SaveWin10Pictures/Program.cs
+++ SaveWin10Pictures/Program.cs
@@ -4,14 +4,14 @@ using System.IO;
 
 namespace SaveWin10Pictures
 {
-  class Program
+  internal class Program
   {
-    static void Main(string[] args)
+    private static void Main()
     {
       Action<string> display = Console.WriteLine;
       List<string> files = new List<string>();
-      
-      string userName = "userName";
+      int counter = 0;
+      const string userName = "userName";
       string imagePath = $@"C:\Users\{userName}\Pictures\fond_ecran";
       foreach (string file in GetFilesFileteredBySize(new DirectoryInfo($@"C:\Users\{userName}\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets"), 100000))
       {
@@ -20,19 +20,23 @@ namespace SaveWin10Pictures
 
       try
       {
-        bool overwirte = false;
+        const bool overwirte = false;
         for (int i = 0; i < files.Count; i++)
         {
-          string source = files[i].ToString();
+          string source = files[i];
           string destination = Path.Combine(imagePath, source) + ".jpg";
           File.Copy(source, destination, overwirte);
+          counter++;
         }
       }
       catch (Exception)
       {
-        // do nothing and continue to next file
+        // do nothing and continue with the next file
       }
 
+      display($"{counter} images have been copied to the picture folder.");
+      display("Press any key to exit:");
+      Console.ReadKey();
     }
 
     public static List<string> GetFilesFileteredBySize(DirectoryInfo directoryInfo, long sizeGreaterOrEqualTo)
