74f4e63c869552b5ba7c9e1ddc9dc9ccc012c627
fred <domaine75@gmail.com>
2019-03-13 21:55:27 +0100

    add code

--- SaveWin10Pictures/Program.cs
+++ SaveWin10Pictures/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace SaveWin10Pictures
 {
@@ -10,6 +8,45 @@ namespace SaveWin10Pictures
   {
     static void Main(string[] args)
     {
+      Action<string> display = Console.WriteLine;
+      List<string> files = new List<string>();
+      
+      string userName = "userName";
+      string imagePath = $@"C:\Users\{userName}\Pictures\fond_ecran";
+      foreach (string file in GetFilesFileteredBySize(new DirectoryInfo($@"C:\Users\{userName}\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets"), 100000))
+      {
+        files.Add(file);
+      }
+
+      try
+      {
+        bool overwirte = false;
+        for (int i = 0; i < files.Count; i++)
+        {
+          string source = files[i].ToString();
+          string destination = Path.Combine(imagePath, source) + ".jpg";
+          File.Copy(source, destination, overwirte);
+        }
+      }
+      catch (Exception)
+      {
+        // do nothing and continue to next file
+      }
+
+    }
+
+    public static List<string> GetFilesFileteredBySize(DirectoryInfo directoryInfo, long sizeGreaterOrEqualTo)
+    {
+      List<string> result = new List<string>();
+      foreach (FileInfo fileInfo in directoryInfo.GetFiles())
+      {
+        if (fileInfo.Length >= sizeGreaterOrEqualTo)
+        {
+          result.Add(fileInfo.FullName);
+        }
+      }
+
+      return result;
     }
   }
 }
