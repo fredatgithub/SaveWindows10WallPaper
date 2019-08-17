4b001e5fbaa4dd32ba7f80c3a6a9ad1be9a3dae0
fred <domaine75@gmail.com>
2019-08-17 15:59:10 +0200

    Upgrade Version number to 1.8

--- SaveWin10Pictures/Properties/AssemblyInfo.cs
+++ SaveWin10Pictures/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("SaveWin10Pictures")]
+[assembly: AssemblyTitle("Save Windows 10 screen saver Pictures")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SaveWin10Pictures")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCompany("Freddy Juhel")]
+[assembly: AssemblyProduct("Save Windows 10 screen saver Pictures")]
+[assembly: AssemblyCopyright("Copyright MIT ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.0.0")]
-[assembly: AssemblyFileVersion("1.7.0.0")]
+[assembly: AssemblyVersion("1.8.0.0")]
+[assembly: AssemblyFileVersion("1.8.0.0")]
