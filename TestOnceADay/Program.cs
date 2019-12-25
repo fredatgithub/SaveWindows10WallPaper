7980ee74517171c2baf702e9f8b62026374f34a0
Fred <domaine75@gmail.com>
2019-12-25 17:35:33 +0100

    Add other code to detect keys

--- TestOnceADay/Program.cs
+++ TestOnceADay/Program.cs
@@ -7,21 +7,83 @@ namespace TestOnceADay
   {
     static void Main(string[] args)
     {
-      int counter = 0;
-      do
+      //int counter = 0;
+      //do
+      //{
+      //  if (DateTime.Now.Hour == 15 && DateTime.Now.Minute == 15 && DateTime.Now.Second == 31)
+      //  {
+      //    counter++;
+      //    Console.WriteLine(counter);
+      //  }
+      //} while (true);
+
+      //// about 2529 or 2752 times within one second
+      //// about 488 508 times within one minute 
+
+      //Console.WriteLine("press any key to exit:");
+      //Console.ReadKey();
+      
+      // code 2
+      ConsoleKeyInfo cki = new ConsoleKeyInfo();
+      while (true)
       {
-        if (DateTime.Now.Hour == 15 && DateTime.Now.Minute == 15 && DateTime.Now.Second == 31)
+        if (WaitOrBreak())
         {
-          counter++;
-          Console.WriteLine(counter);
+          break;
         }
-      } while (true);
 
-      // about 2529 or 2752 times within one second
-      // about 488 508 times within one minute 
+        //your main code
+      }
 
-      Console.WriteLine("press any key to exit:");
-      Console.ReadKey();
+      bool WaitOrBreak()
+      {
+        if (Console.KeyAvailable) cki = Console.ReadKey(true);
+        if (cki.Key == ConsoleKey.Spacebar)
+        {
+          Console.Write("Spacebar has been pressed ...");
+          while (Console.KeyAvailable == false)
+          {
+            Thread.Sleep(250); Console.Write(".");
+          }
+
+          Console.WriteLine();
+          Console.ReadKey(true);
+          cki = new ConsoleKeyInfo();
+        }
+
+        if (cki.Key == ConsoleKey.S)
+        {
+          Console.Write("S has been pressed ...");
+          while (Console.KeyAvailable == false)
+          {
+            Thread.Sleep(250); Console.Write(".");
+          }
+
+          Console.WriteLine();
+          Console.ReadKey(true);
+          cki = new ConsoleKeyInfo();
+        }
+
+        if (cki.Key == ConsoleKey.T)
+        {
+          Console.Write("T has been pressed ...");
+          while (Console.KeyAvailable == false)
+          {
+            Thread.Sleep(250); Console.Write(".");
+          }
+
+          Console.WriteLine();
+          Console.ReadKey(true);
+          cki = new ConsoleKeyInfo();
+        }
+
+        if (cki.Key == ConsoleKey.Escape)
+        {
+          return true;
+        }
+
+        return false;
+      }
     }
   }
 }
