b8f7c3fff47095b450261f597573971ba4a09db8
Fred <domaine75@gmail.com>
2021-05-19 20:56:07 +0200

    Increase version and update copyright

--- SaveWin10Pictures/Properties/AssemblyInfo.cs
+++ SaveWin10Pictures/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Freddy Juhel")]
 [assembly: AssemblyProduct("Save Windows 10 screen saver Pictures")]
-[assembly: AssemblyCopyright("Copyright MIT ©  2020")]
+[assembly: AssemblyCopyright("Copyright MIT ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.9.0.0")]
-[assembly: AssemblyFileVersion("1.9.0.0")]
+[assembly: AssemblyVersion("1.9.1.0")]
+[assembly: AssemblyFileVersion("1.9.1.0")]
